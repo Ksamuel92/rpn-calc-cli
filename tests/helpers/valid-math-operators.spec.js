749d849c3abb8e97f206ec6d20a99ffa6582e3ae
Kyle Samuel <ksamuel92@gmail.com>
2022-01-18 09:54:29 -0500

    add test for valid-math-operators

---
+++ tests/helpers/valid-math-operators.spec.js
@@ -0,0 +1,24 @@
+const {
+	validMathOperators,
+	validOperatorsArray
+} = require('../../src/helpers/valid-math-operators');
+
+describe('valid-math-operators', () => {
+	test('validMathOperators should invoke a function when a valid operator is provided', () => {
+		let mathOperator = '+';
+		let [a, b] = [3, 3];
+		let addition = validMathOperators[mathOperator];
+		let answer = addition(a, b);
+		expect(answer).toEqual(6);
+	});
+	test('validOperatorsArray should return an array of keys from an object.', () => {
+		let mathObject = {
+			'+': 'addition',
+			'-': 'subtraction',
+			'/': 'division',
+			'*': 'multiply'
+		};
+		let array = validOperatorsArray(mathObject);
+		expect(array).toEqual(['+', '-', '/', '*']);
+	});
+});
