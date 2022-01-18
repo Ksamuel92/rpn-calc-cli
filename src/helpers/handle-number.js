ab049819d42a0b9912b7ecbab25ad9be4eca5beb
Kyle Samuel <ksamuel92@gmail.com>
2022-01-17 22:13:34 -0500

    fix: :bug: Implement NaN handling with handleNumber helper
    Prevents NaN values to be pushed onto the stack

--- src/helpers/handle-number.js
+++ src/helpers/handle-number.js
@@ -1,8 +1,8 @@
 // Takes integer, whether a number or a string, and an array. Converts integer to a number and pushes it onto the array provided.
 
 const handleNumber = (element, array) => {
-	const validInteger = parseFloat(element);
-	array.push(validInteger);
+	const parsedElement = parseFloat(element);
+	isNaN(parsedElement) ? null : array.push(parsedElement);
 };
 
 module.exports = {
