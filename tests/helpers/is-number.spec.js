6b33cd8ed7cfba0faad3cadb6e3b0ad94577f368
Kyle Samuel <ksamuel92@gmail.com>
2022-01-18 08:56:12 -0500

    add is-number test

---
+++ tests/helpers/is-number.spec.js
@@ -0,0 +1,16 @@
+const { isNumber } = require('../../src/helpers/is-number');
+
+describe('isNumber', () => {
+	it('returns true when a number type is a number', () => {
+		let number = 3;
+		expect(isNumber(number)).toBe(true);
+	}),
+		it('returns true when a string number is a number', () => {
+			let stringNumber = '3';
+			expect(isNumber(stringNumber)).toBe(true);
+		}),
+		it('returns false when a string is not a number', () => {
+			let string = '3wow';
+			expect(isNumber(string)).toBe(false);
+		});
+});
