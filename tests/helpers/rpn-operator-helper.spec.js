76ce2f84151e72c0b15c877f15b2081adce6b728
Kyle Samuel <ksamuel92@gmail.com>
2022-01-18 10:14:17 -0500

    add rpn-operator-helper test

---
+++ tests/helpers/rpn-operator-helper.spec.js
@@ -0,0 +1,28 @@
+const {
+	isOperator,
+	pushCalculationToStack
+} = require('../../src/helpers/rpn-operator-helper');
+const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+describe('rpn-operator-helper', () => {
+	test('isOperator  will return true if the operator provided is valid', () => {
+		let operator = '+';
+		let boolean = isOperator(operator);
+		expect(boolean).toBe(true);
+	}),
+		test('pushCalculationToStack will return an array with the calculated value', () => {
+			let operator = '+';
+			let stack = [3, 3];
+			let newStack = pushCalculationToStack(operator, stack);
+			expect(newStack).toEqual([6]);
+		});
+	test('pushCalculationToStack should log an error if operator is invalid or the stack contains less than two elements', () => {
+		let operator = '+';
+		let stack = [3];
+		pushCalculationToStack(operator, stack);
+		expect(console.error).toBeCalledTimes(1);
+		expect(console.error).toHaveBeenLastCalledWith(
+			'Check if stack contains two elements and has a valid operator'
+		);
+	});
+});
