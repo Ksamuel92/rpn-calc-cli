fde973b64e537c7f0dc7991ea4326ff98730857d
Kyle Samuel <ksamuel92@gmail.com>
2022-01-17 22:21:50 -0500

    refactor calc jest test with describe()

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,43 +1,44 @@
-const parseUserInput = require('../lib/calc');
-
-test('Expect 3 3 + to equal 6', () => {
-	const answerObject = parseUserInput([3, 3, '+']);
-	expect(answerObject).toEqual({
-		currentAnswer: 6,
-		currentCallStack: [6]
+const parseUserInput = require('../src/calc');
+describe('calc.js', () => {
+	it('Expects 3 3 + to equal 6', () => {
+		const answerObject = parseUserInput([3, 3, '+']);
+		expect(answerObject).toEqual({
+			currentAnswer: 6,
+			currentStack: [6]
+		});
 	});
-});
 
-test('Expect 3 3 + to equal 6 iteratively', () => {
-	const {
-		currentCallStack: firstIterationCS,
-		currentAnswer: firstIterationAnswer
-	} = parseUserInput([3]);
+	it('Expects 3 3 + to equal 6 iteratively', () => {
+		const {
+			currentStack: firstIterationCS,
+			currentAnswer: firstIterationAnswer
+		} = parseUserInput([3]);
 
-	const { currentCallStack: secondIterationCS } = parseUserInput(
-		[firstIterationAnswer],
-		firstIterationCS
-	);
+		const { currentStack: secondIterationCS } = parseUserInput(
+			[firstIterationAnswer],
+			firstIterationCS
+		);
 
-	const finalAnswerObject = parseUserInput(['+'], secondIterationCS);
+		const finalAnswerObject = parseUserInput(['+'], secondIterationCS);
 
-	expect(finalAnswerObject).toEqual({
-		currentAnswer: 6,
-		currentCallStack: [6]
+		expect(finalAnswerObject).toEqual({
+			currentAnswer: 6,
+			currentStack: [6]
+		});
 	});
-});
 
-test('Expect 5 5 5 8 + + - to equal -13', () => {
-	const answerObject = parseUserInput([5, 5, 5, 8, '+', '+', '-']);
-	expect(answerObject).toEqual({
-		currentAnswer: -13,
-		currentCallStack: [-13]
+	it('Expects 5 5 5 8 + + - to equal -13', () => {
+		const answerObject = parseUserInput([5, 5, 5, 8, '+', '+', '-']);
+		expect(answerObject).toEqual({
+			currentAnswer: -13,
+			currentStack: [-13]
+		});
 	});
-});
 
-test("Expect '3wow' to throw invalid user input", () => {
-	expect(parseUserInput([3, 3, '3wow'])).toEqual({
-		currentAnswer: 'Invalid user input',
-		currentCallStack: [3, 3]
+	it("Expects '3wow' to throw invalid user input", () => {
+		expect(parseUserInput([3, 3, '3wow'])).toEqual({
+			currentAnswer: 'Invalid user input',
+			currentStack: [3, 3]
+		});
 	});
 });
