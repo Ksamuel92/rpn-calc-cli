4e6e9a59b02a72f0b660b39d1330332ad1ca9b5e
Kyle Samuel <ksamuel92@gmail.com>
2022-01-18 12:47:56 -0500

    feat: :sparkles: stub out index cli integration test
    Tests are stubbed due to knowledge gap on emulating CLI prompts in Jest without mocks

---
+++ tests/index.test.js
@@ -0,0 +1,32 @@
+const parseUserInput = require('../src/calc');
+const { stdin } = require('mock-stdin');
+
+describe('RPN Calculator', () => {
+	beforeAll(() => {
+		io = stdin();
+	});
+
+	it('should prompt for input', async () => {
+		let mockPrompt = jest
+			.fn()
+			.mockReturnValue({ userInputArray: [3, 3, '+'] });
+		let userInput = '3 3 +';
+		process.nextTick(() => {
+			io.send(`${userInput}\r`);
+		});
+		const result = await mockPrompt();
+		expect(result).toEqual({ userInputArray: [3, 3, '+'] });
+	});
+	it('should calculate the resulting array and return the current answer and current callstack', async () => {
+		let { userInputArray } = { userInputArray: [3, 3, '+'] };
+		console.log(parseUserInput);
+		let calculation = await parseUserInput(userInputArray);
+		expect(calculation).toEqual({
+			currentAnswer: 6,
+			currentStack: [6]
+		});
+	});
+	afterAll(() => {
+		io.restore();
+	});
+});
