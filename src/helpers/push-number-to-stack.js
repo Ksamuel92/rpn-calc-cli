394ef19a7b8ffcfbf59ca1bc9ef4c59cd750613f
Kyle Samuel <ksamuel92@gmail.com>
2022-01-19 14:42:37 -0500

    change  array parameter to stack

--- src/helpers/push-number-to-stack.js
+++ src/helpers/push-number-to-stack.js
@@ -1,12 +1,12 @@
 // Takes integer, whether a number or a string, and an array. Converts integer to a number and pushes it onto the array provided.
 
-const pushNumberToStack = (element, array) => {
+const pushNumberToStack = (element, stack) => {
 	const parsedElement = parseFloat(element);
 	if (isNaN(parsedElement)) {
 		return console.error('Not a valid number');
 	} else {
-		array.push(parsedElement);
-		return array;
+		stack.push(parsedElement);
+		return stack;
 	}
 };
 
